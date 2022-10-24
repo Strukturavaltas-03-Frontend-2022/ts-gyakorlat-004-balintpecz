88e537c9d93fccdced6d1a1de3559b8a456fd98d
Pecz <p.balint1000@gmail.com>
2022-10-25 00:24:48 +0200

    Done.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from "./hero";
+import { TransformerHero } from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,12 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+ export const humans: HumanHero[] = [
+    new HumanHero(1, "Test Elek", "male", 63, 32 ),
+    new HumanHero(2, "Test Erika", "female", 77, 42),
+    new HumanHero(3, "Test Elek", "male", 90, 60)
+  ];
+  
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +23,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+ export const transformers: any[] = [
+    {id:1, name: "transform", wings:0, wheels:2, clan: 'alma'},
+    {id:2, name: "transform", wings:1, wheels:5, clan: 'beka'},
+    {id:3, name: "transform", wings:2, wheels:6, clan: 'szolo'},
+  ];
